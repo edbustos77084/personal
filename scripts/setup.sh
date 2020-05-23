512c644499958b81692a0ce92556b9da4522bf2f
Bustos <edward.bustos@hpe.com>
2020-05-22 21:04:00 -0500

    update script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -31,7 +31,8 @@ echo "$(tput setaf 4) $(tput setab 7)Download Vagrant 2.2.9...$(tput sgr 0)"
 sudo wget https://releases.hashicorp.com/vagrant/2.2.9/vagrant_2.2.9_x86_64.deb
 echo "done..."
 echo ""
-echo "sleep for 10 seconds"
+echo "Pause 10 seconds; You may have to change Directory permissions where you want to launch Vagrant"
+echo "If Vagrant does not start '$ sudo chown -R <user> <directory>' "
 sudo sleep 10
 echo ""
 
