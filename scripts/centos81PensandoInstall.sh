c5d687340450986d2c5d06a8d2179c8c0bdc4ed2
Bustos <edward.bustos@hpe.com>
2020-06-06 15:42:03 -0500

    update script

--- scripts/centos81PensandoInstall.sh
+++ scripts/centos81PensandoInstall.sh
@@ -60,7 +60,7 @@ echolog "$(tput setaf 4) $(tput setab 7)Install Kernel Headers...$(tput sgr 0)"
 sudo yum -y install kernel-headers || exit 1
 echo "done.."
 echolog "$(tput setaf 4) $(tput setab 7)Update Kernel...$(tput sgr 0)"
-sudo yum -y install kernel || exit 1
+sudo yum -y install kernel || exit 1 
 echo "done.."
 echolog "$(tput setaf 4) $(tput setab 7)build-essentials...$(tput sgr 0)"
 sudo yum -y install gcc gcc-c++ kernel-devel make || exit 1
@@ -68,10 +68,12 @@ echo "done.."
 echolog "$(tput setaf 4) $(tput setab 7)Development Tools...$(tput sgr 0)"
 sudo yum -y groupinstall "Development Tools" || exit 1
 echo "done.."
-echolog "$(tput setaf 4) $(tput setab 7)Install Kernel-devel package...$(tput sgr 0)"
-sudo wget https://rpmfind.net/linux/centos/8.1.1911/BaseOS/x86_64/os/Packages/kernel-devel-4.18.0-147.8.1.el8_1.x86_64.rpm || exit 1
-sudo rpm -ivh kernel-devel-4.18.0-147.8.1.el8_1.x86_64.rpm || exit 1
-echo "done"
+
+# I don't we need this 
+#echolog "$(tput setaf 4) $(tput setab 7)Install Kernel-devel package...$(tput sgr 0)"
+#sudo wget https://rpmfind.net/linux/centos/8.1.1911/BaseOS/x86_64/os/Packages/kernel-devel-4.18.0-147.8.1.el8_1.x86_64.rpm || exit 1
+#sudo rpm -ivh kernel-devel-4.18.0-147.8.1.el8_1.x86_64.rpm || exit 1
+#echo "done"
 
 
 echolog "$(tput setaf 4) $(tput setab 7)Install Requirements for Driver update...$(tput sgr 0)"
@@ -89,19 +91,18 @@ echolog "$(tput setaf 4) $(tput setab 7)Get the Pensando Driver Package...$(tput
 sudo wget http://${VAR_NGINX}${VAR_PEN_DRIVER} 
 fi
 
-if [ -f drivers-linux-eth.tar.xz ]; then
-echolog "$(tput setaf 4) $(tput setab 7)Install the Pensando driver...$(tput sgr 0)"
+echolog "$(tput setaf 4) $(tput setab 7)Extract the Pensando Driver...$(tput sgr 0)"
 sudo xz -d ${VAR_PEN_DRIVER}
-sudo tar xvf ${VAR_PEN_DRIVER}
-sudo cd drivers-linux/
+sudo tar xvf drivers-linux-eth.tar
+
+
+echolog "$(tput setaf 4) $(tput setab 7)Install the Pensando driver...$(tput sgr 0)"
+cd drivers-linux-eth/
 sudo ./build.sh 
 echo "pause 5 seconds"
 sudo sleep 5
-sudo insmod eth/ionic/ionic.ko 
-else
-echolog "Driver is unavailable...verify it was donwloaded correctly"
-echo "done.."
-fi
+sudo insmod drivers/eth/ionic/ionic.ko 
+
 
 echolog "$(tput setaf 4) $(tput setab 7)Test for Driver settings...$(tput sgr 0)"
 fileIonic=/etc/sysconfig/modules/ionic.modules
@@ -116,7 +117,7 @@ fi
 
 sudo chmod 755 /etc/sysconfig/modules/ionic.modules
 
-echolog "done..."
+echolog "Changed ionic permissions.."
 
 
 echolog "$(tput setaf 4) $(tput setab 7)DONE...Confirm Pensando driver is loaded...$(tput sgr 0)"
