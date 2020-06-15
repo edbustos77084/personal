02c64cc9dce849d80fd9254ce38671894fe0a2ce
Bustos <edward.bustos@hpe.com>
2020-06-15 18:06:57 -0500

    updated docker script

--- scripts/centos81Docker.sh
+++ scripts/centos81Docker.sh
@@ -2,15 +2,19 @@
 set -e
 set -o pipefail
 
-# Using this script on the LR1 which requires different proxy settings that LR0
-# NOTE: Assume you are running this from your home directory or cd ~...for the driver to load. 
+# Edward Bustos 06/15/2020
+# Be sure you have the correct proxy settings
+# Assume you are running this from your home directory as ROOT or cd ~
+# This script should install Docker on your CentOS 8.1 image
+# There are known conflict issues with PODMAN, so if you have that installed, you may have to remove those files. 
+# See https://github.com/containers/libpod/issues/4791 describing the issue 
 
 NOW=$(date +"%T-%D")
 VAR_PROXY="http://proxy.compaq.com:8080"
 VAR_PEN_DRIVER="drivers-linux-eth.tar.xz"
 VAR_NGINX="15.115.118.72:8081/"
+log_file="log_DockerInstall.log"
 
-log_file="log_file.log"
 echolog()
 (
  echo "$@"
@@ -24,7 +28,7 @@ echolog "$(tput setaf 4) $(tput setab 7)Test for Proxy settings...$(tput sgr 0)"
 fileEnv=/etc/environment
 if grep -q proxy $fileEnv;
 then
-echo "Proxy appears to be available"
+echolog "Proxy appears to be available"
 else
 echolog "Ooops appears to be missing, let's write it"
 sudo cat>>/etc/environment<<EOF 
@@ -33,40 +37,39 @@ https_proxy="${VAR_PROXY}"
 no_proxy="localhost,127.0.0.1,::1,.hpqcorp.net,hpecorp.net"
 EOF
 fi
-echo "done..."
+echolog "done..."
 
 
 echolog "$(tput setaf 4) $(tput setab 7)Test for YUM settings..$(tput sgr 0)"
 fileYum=/etc/yum.conf
 if grep -q proxy $fileYum
 then
-echo "Proxy appears to be available"
+echolog "Proxy appears to be available"
 else
 echolog "Ooops appears to be missing, let's write it"
 sudo cat>>/etc/yum.conf<<EOF
 proxy="${VAR_PROXY}"
 EOF
 fi
-echo "done.."
+echolog "done.."
 
 
 echolog "$(tput setaf 4) $(tput setab 7)Install Docker Dependencies...$(tput sgr 0)"
 sudo yum install -y yum-utils device-mapper-persistent-data lvm2
-echo "done.."
+echolog "done.."
 
 echolog "$(tput setaf 4) $(tput setab 7)Install Repository to CentOS...$(tput sgr 0)"
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-#sudo yum install -y http://mirror.centos.org/centos/7/extras/x86_64/Packages/container-selinux-2.55-1.el7.noarch.rpm
-echo "done.."
+echolog "done.."
 
 echolog "$(tput setaf 4) $(tput setab 7)Install Docker...$(tput sgr 0)"
-sudo yum install docker-ce docker-ce-cli --nobest
-echo "done.."
+sudo yum install docker-ce docker-ce-cli -y --nobest
+echolog "done.."
 
 
 echolog "$(tput setaf 4) $(tput setab 7)Create systemd directory for docker environment...$(tput sgr 0)"
 sudo mkdir -p /etc/systemd/system/docker.service.d
-echo "done.."
+echolog "done.."
 
 
 echolog "$(tput setaf 4) $(tput setab 7)Create file called http-proxy.conf...$(tput sgr 0)"
@@ -76,7 +79,7 @@ sudo cat >>/etc/systemd/system/docker.service.d/http-proxy.conf<<EOF
 Environment="HTTP_PROXY=${VAR_PROXY}"
 Environment="NO_PROXY=localhost,.hpecorp.net"
 EOF
-echo "done.."
+echolog "done.."
 
 
 echolog "$(tput setaf 4) $(tput setab 7)Create a file called https-proxy.conf...$(tput sgr 0)"
@@ -85,7 +88,7 @@ sudo cat >>/etc/systemd/system/docker.service.d/https-proxy.conf<<EOF
 [Service]
 Environment="HTTPS_PROXY=${VAR_PROXY}"
 EOF
-echo "done.."
+echolog "done.."
 
 echolog "$(tput setaf 4) $(tput setab 7)Manage Docker Service...$(tput sgr 0)"
 sudo systemctl daemon-reload
@@ -94,4 +97,6 @@ sudo systemctl enable docker
 
 
 
-echolog "$(tput setaf 4) $(tput setab 7)DONE...Confirm Docker is working...$(tput sgr 0)"
+echolog "$(tput setaf 4) $(tput setab 7)DONE...Confirm Docker is working..........................$(tput sgr 0)"
+echolog "$(tput setaf 4) $(tput setab 7)run this command: docker container run hello-world........$(tput sgr 0)"
+echolog "$(tput setaf 4) $(tput setab 7)sudo systemctl show --property=Environment docker..........$(tput sgr 0)"
