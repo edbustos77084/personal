5e4bf6d77010316d5c8026a394c04872ab966d38
Bustos <edward.bustos@hpe.com>
2020-06-18 11:47:01 -0500

    updated proxy script

---
+++ scripts/centos81test4Proxy.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+set -e
+set -o pipefail
+
+# Edward Bustos 
+# SHELL Script to bootstrap CentOS 8.1 (build 1911) after a base OS install.
+# Test for Proxy and if not present set it.  
+
+
+NOW=$(date +"%T-%D")
+VAR_PROXY="http://proxy.compaq.com:8080"
+VAR_PEN_DRIVER="drivers-linux-eth.tar.xz"
+VAR_NGINX="15.115.118.72:8081/"
+log_file="logProxyKernelheaders.log"
+
+echolog()
+(
+ echo "$@"
+ echo "$@" >> $log_file
+)
+
+echolog "Starting log $NOW...."
+
+
+echolog "$(tput setaf 4) $(tput setab 7)Test for Proxy settings...$(tput sgr 0)"
+fileEnv=/etc/environment
+if grep -q proxy $fileEnv;
+then
+echolog "Proxy appears to be available"
+else
+echolog "Ooops appears to be missing, let's write it"
+sudo cat>>/etc/environment<<EOF 
+http_proxy="${VAR_PROXY}"
+https_proxy="${VAR_PROXY}"
+no_proxy="localhost,127.0.0.1,::1,.hpqcorp.net,hpecorp.net"
+EOF
+fi
+echolog "done..."
+
+
+echolog "$(tput setaf 4) $(tput setab 7)Test for YUM settings..$(tput sgr 0)"
+fileYum=/etc/yum.conf
+if grep -q proxy $fileYum
+then
+echolog "Proxy appears to be available"
+else
+echolog "Ooops appears to be missing, let's write it"
+sudo cat>>/etc/yum.conf<<EOF
+proxy="${VAR_PROXY}"
+EOF
+fi
+echolog "done.."
+
+
+echolog "$(tput setaf 4) $(tput setab 7)Verify you have Proxy.........................$(tput sgr 0)"
+
